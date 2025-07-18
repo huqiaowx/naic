e1d623db1601390d9485d31b6a5c401ce685cdc0
huqiaowx <janvo249@gmail.com>
2025-07-18 20:54:07 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// Service Worker для Скибидиbike
+const CACHE_NAME = 'skibidibike-cache-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/login.html',
+  '/success.html',
+  '/styles.css',
+  '/images/hero-bg.jpg',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
